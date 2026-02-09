8f51cfce5a2272190c7d61da3be3e9d27dd96c91
jamescarl711 <okarinxkurisu@gmail.com>
2026-02-10 02:22:53 +0800

    Delete ThesisCapstone directory

--- ThesisCapstone/resources/js/Components/SectionTitle.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <h3 class="text-lg font-semibold text-gray-700 mb-4">{{ title }}</h3>
-</template>
-
-<script setup>
-defineProps({
-  title: { type: String, required: true }
-})
-</script>
